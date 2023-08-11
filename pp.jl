b50921606e726c0b3d45cd182e50703b1147a78a
jakob prossliner <hansjakob.leiterprossliner@gmail.com>
2023-08-11 20:06:40 +0200

    nlobjective

--- pp.jl
+++ pp.jl
@@ -1,15 +1,15 @@
 using JuMP
 using GLPK
-m = Model(GLPK.Optimizer)
+using Ipopt
 
 prices = [1, 1.4, 3, 3, 2, 0.3, -1]
 n = length(prices)
-model = Model(GLPK.Optimizer)
+model = Model(Ipopt.Optimizer)
 
 @variable(model, 0 .<= production[i = 1:n] .<= 1)
 @constraint(model, sum(production[i] for i in 1:n) <= 6)
 @constraint(model, -0.3 .<= diff(production) .<= 0.3)
-@objective(model, Max, sum(production[i] * prices[i] for i in 1:n))
+@NLobjective(model, Max, sum(production[i] * prices[i] for i in 1:n)/sum(production[i] for i in 1:n))
 
 print(model)
 
@@ -19,4 +19,4 @@ print(model)
 
 solution_summary(model)
 
-value.(production)
+round.(value.(production); digits = 4)
