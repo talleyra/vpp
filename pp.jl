a42dd768d87f7e09dfebb0880d39b5bdf03437e5
jakob prossliner <hansjakob.leiterprossliner@gmail.com>
2023-08-11 20:17:16 +0200

    Revert "nlobjective"
    This reverts commit b50921606e726c0b3d45cd182e50703b1147a78a.

--- pp.jl
+++ pp.jl
@@ -1,15 +1,15 @@
 using JuMP
 using GLPK
-using Ipopt
+m = Model(GLPK.Optimizer)
 
 prices = [1, 1.4, 3, 3, 2, 0.3, -1]
 n = length(prices)
-model = Model(Ipopt.Optimizer)
+model = Model(GLPK.Optimizer)
 
 @variable(model, 0 .<= production[i = 1:n] .<= 1)
 @constraint(model, sum(production[i] for i in 1:n) <= 6)
 @constraint(model, -0.3 .<= diff(production) .<= 0.3)
-@NLobjective(model, Max, sum(production[i] * prices[i] for i in 1:n)/sum(production[i] for i in 1:n))
+@objective(model, Max, sum(production[i] * prices[i] for i in 1:n))
 
 print(model)
 
@@ -19,4 +19,4 @@ print(model)
 
 solution_summary(model)
 
-round.(value.(production); digits = 4)
+value.(production)
